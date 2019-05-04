b3d541a054289f9d7b342e462bfa342e29b4f51d
Alex <alex.pilkevych@gmail.com>
2019-05-04 12:43:13 +0300

    [R] test query

--- app/tests/test_structure.py
+++ app/tests/test_structure.py
@@ -9,3 +9,7 @@ def area_index():
 
 def test_bulk_create(area_index):
     area_index.bulk_create([(-24.5, 134.8, 20, 'Australia')])
+
+
+def test_query(area_index):
+    assert area_index.query(0, 0) == []
