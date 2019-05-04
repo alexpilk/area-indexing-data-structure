8086631d8677a52db5a564f9957a1bfb5387a459
Alex <alex.pilkevych@gmail.com>
2019-05-04 12:32:36 +0300

    [G] bulk_create interface

--- app/structure.py
+++ app/structure.py
@@ -1,2 +1,8 @@
 class AreaIndex:
-    pass
+
+    def bulk_create(self, areas):
+        """
+        :param list(tuple) areas: area description should contain latitude, longitude, radius and id.
+
+        >>> AreaIndex().bulk_create([(-24.5, 134.8, 20, 'Australia')])
+        """
