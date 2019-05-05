2d485af6357d506da7ea184610b6f573c37ef8ae
Alex <alex.pilkevych@gmail.com>
2019-05-05 12:55:42 +0300

    [G] implemented query logic

--- app/structure.py
+++ app/structure.py
@@ -1,11 +1,16 @@
 class AreaIndex:
 
+    def __init__(self):
+        self.areas = {}
+
     def bulk_create(self, areas):
         """
         :param list(tuple) areas: area description should contain latitude, longitude, radius and id.
 
         >>> AreaIndex().bulk_create([(-24.5, 134.8, 20, 'Australia')])
         """
+        for area in areas:
+            self.areas[area[3]] = area
 
     def query(self, lat, long):
         """
@@ -18,7 +23,7 @@ class AreaIndex:
         []
 
         """
-        return []
+        return [area_id for area_id, area in self.areas.items() if self._point_in_area((lat, long), area)]
 
     def _point_in_area(self, point, area):
         lat = area[0]
