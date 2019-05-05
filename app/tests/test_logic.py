2e6bd8b2c24c84b51bdfffac869ccf6dba422cee
Alex <alex.pilkevych@gmail.com>
2019-05-05 13:28:30 +0300

    [R] test fails on areas with same id

--- app/tests/test_logic.py
+++ app/tests/test_logic.py
@@ -1,5 +1,7 @@
-from app.tests.fixtures import area_index
+import pytest
+
 from app.structure import Area
+from app.tests.fixtures import area_index
 
 
 def test_point_in_area(area_index):
@@ -33,3 +35,9 @@ def test_query_point_in_two_areas(area_index):
 def test_query_point_outside_two_areas(area_index):
     area_index.bulk_create([(-10, 0, 10, 'Area 1'), (10, 0, 10, 'Area 2')])
     assert area_index.query(0, 1) == []
+
+
+def test_fails_to_add_areas_with_same_ids(area_index):
+    from app.structure import DuplicateIds
+    with pytest.raises(DuplicateIds):
+        area_index.bulk_create((0, 0, 0, 'Area 1'), (1, 1, 1, 'Area 1'))
