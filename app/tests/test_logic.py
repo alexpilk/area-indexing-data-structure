c30a84c0f1817ffa703c9632383877001ada3fb1
Alex <alex.pilkevych@gmail.com>
2019-05-05 12:40:20 +0300

    [R] test for _point_in_area edge case

--- app/tests/test_logic.py
+++ app/tests/test_logic.py
@@ -11,3 +11,9 @@ def test_point_not_in_area(area_index):
     point = (0, 0)
     area = (-20, 20, 20, 'Area 1')
     assert not area_index._point_in_area(point, area)
+
+
+def test_point_on_edge(area_index):
+    point = (-20, 0)
+    area = (-20, 20, 20, 'Area 1')
+    assert area_index._point_in_area(point, area)
