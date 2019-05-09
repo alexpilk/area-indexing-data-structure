8f2fd78e118e1078d21f82dce9d6180feca7ae03
Pilkevych Oleksii <oleksii.pilkevych@nokia.com>
2019-05-09 09:29:01 +0200

    [R] Test area inserted before

--- app/tests/test_range.py
+++ app/tests/test_range.py
@@ -7,3 +7,12 @@ def test_range_is_created():
     main_range = Range(area, area.min_lat, area.max_lat)
     assert main_range.space == [-20, 40]
     assert main_range.areas == {(-20, 40): [area]}
+
+
+def test_area_inserted_before():
+    area_1 = Area(10, 20, 30, 'Area 1')
+    area_2 = Area(-30, 20, 5, 'Area 2')
+    main_range = Range(area_1, area_1.min_lat, area_1.max_lat)
+    main_range.add(area_2, area_2.min_lat, area_2.max_lat)
+    assert main_range.space == [-35, -25, -20, 40]
+    assert main_range.areas == {(-20, 40): [area_1], (-35, -25): [area_2]}
