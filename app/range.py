52e0217523a1c8e482807316db5e29629a41edbd
Pilkevych Oleksii <oleksii.pilkevych@nokia.com>
2019-05-09 14:19:40 +0200

    [B] Split Range.add into multiple methods

--- app/range.py
+++ app/range.py
@@ -3,18 +3,23 @@ from collections import defaultdict
 
 class Range:
 
-    def __init__(self, area, x, y):
+    def __init__(self, area, start, end):
         self.areas = defaultdict(list)
-        self.space = [x, y]
-        self.areas[(x, y)].append(area)
+        self.space = [start, end]
+        self.areas[(start, end)].append(area)
 
-    def add(self, area, x, y):
+    def add(self, area, start, end):
         first = self.space[0]
         last = self.space[-1]
-        if y < first:
-            self.space = [x, y] + self.space
-            self.areas[(x, y)] = [area]
-        elif x > last:
-            self.space = self.space + [x, y]
-            self.areas[(x, y)] = [area]
+        if end < first:
+            self._add_before(area, start, end)
+        elif start > last:
+            self._add_after(area, start, end)
 
+    def _add_before(self, area, start, end):
+        self.space = [start, end] + self.space
+        self.areas[(start, end)] = [area]
+
+    def _add_after(self, area, start, end):
+        self.space = self.space + [start, end]
+        self.areas[(start, end)] = [area]
