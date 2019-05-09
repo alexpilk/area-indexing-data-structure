38b82cf7f4442b9cdb261b0dc097390698f22e2a
Pilkevych Oleksii <oleksii.pilkevych@nokia.com>
2019-05-09 14:15:45 +0200

    [G] Test area inserted after

--- app/range.py
+++ app/range.py
@@ -10,6 +10,11 @@ class Range:
 
     def add(self, area, x, y):
         first = self.space[0]
+        last = self.space[-1]
         if y < first:
             self.space = [x, y] + self.space
             self.areas[(x, y)] = [area]
+        elif x > last:
+            self.space = self.space + [x, y]
+            self.areas[(x, y)] = [area]
+
